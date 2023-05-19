a51143e73408a0e18bf4cf34801387b601b17c3c
F1686352 <hzlh-mis-xiao.lh@foxconn.com>
2023-05-19 15:47:13 +0800

    Refactor AuthorMapper add commits

--- src/test/java/org/apache/ibatis/domain/blog/mappers/AuthorMapper.java
+++ src/test/java/org/apache/ibatis/domain/blog/mappers/AuthorMapper.java
@@ -27,20 +27,50 @@ import org.apache.ibatis.session.ResultHandler;
 
 public interface AuthorMapper {
 
+  /**
+   * 查询所有作者
+   * @return List<Author> List集合
+   */
   List<Author> selectAllAuthors();
 
+  /**
+   * 查询所有作者
+   * @return Set<Author> Set集合
+   */
   Set<Author> selectAllAuthorsSet();
 
+  /**
+   * 查询所有作者
+   * @return Vector<Author> Vector集合
+   */
   Vector<Author> selectAllAuthorsVector();
 
+  /**
+   * 查询所有作者
+   * @return LinkedList<Author> LinkedList集合
+   */
   LinkedList<Author> selectAllAuthorsLinkedList();
 
+  /**
+   * 查询所有作者
+   * @return Author[]  数组
+   */
   Author[] selectAllAuthorsArray();
 
   void selectAllAuthors(ResultHandler handler);
 
+  /**
+   * 查询作者单条记录
+   * @param id 主键id
+   * @return Author 单条记录
+   */
   Author selectAuthor(int id);
 
+  /**
+   * 查询作者
+   * @param id 主键id
+   * @return LinkedHashMap<String, Object>  map集合
+   */
   LinkedHashMap<String, Object> selectAuthorLinkedHashMap(int id);
 
   void selectAuthor(int id, ResultHandler handler);
@@ -48,10 +78,24 @@ public interface AuthorMapper {
   @Select("select")
   void selectAuthor2(int id, ResultHandler handler);
 
+  /**
+   * 插入作者单条记录
+   * @param author  Author
+   */
   void insertAuthor(Author author);
 
+  /**
+   * 删除作者单条记录
+   * @param id 主键id
+   * @return int 删除的记录数
+   */
   int deleteAuthor(int id);
 
+  /**
+   * 更新作者单条记录
+   * @param author Author
+   * @return int 更新的记录数
+   */
   int updateAuthor(Author author);
 
 }
