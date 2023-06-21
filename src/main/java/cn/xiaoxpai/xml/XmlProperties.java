8e0c9f3a3f732cbd56effa7afa0f698ab9dd9480
F1686352 <hzlh-mis-xiao.lh@foxconn.com>
2023-06-21 15:06:43 +0800

    [xml] 查询插入标签属性描述 (#3)

---
+++ src/main/java/cn/xiaoxpai/xml/XmlProperties.java
@@ -0,0 +1,35 @@
+package cn.xiaoxpai.xml;
+
+public class XmlProperties {
+     /*
+    <resultMap id="BlogResultMap" type="Blog">
+    <constructor>
+      <idArg column="blog_id" name="id"/>
+      <arg column="blog_title" name="title"/>
+    </constructor>
+    <collection property="posts" resultMap="PostResultMap"/>
+  </resultMap>
+   collection:解释：collection元素用于定义集合属性的映射关系，它有两个属性，property和ofType，property用于指定集合属性的名字，ofType用于指定集合中元素的类型。
+    1、property：指定集合属性的名字，它的值是一个字符串，表示集合属性的名字。
+    2、ofType：指定集合中元素的类型，它的值是一个字符串，表示集合中元素的类型。
+    3、select：指定一个查询语句，用于查询集合中的元素。
+    4、idResultMap：指定一个结果映射，用于将查询结果映射到集合中的元素。
+    5、resultSet：指定一个结果集，用于将查询结果映射到集合中的元素。
+    6、fetchType：指定集合的加载策略，它的值是一个字符串，可选值有lazy和eager，默认值是lazy。
+    7、notNullColumn：指定一个列名，用于判断集合中的元素是否为空。
+    8、columnPrefix：指定一个字符串，用于给集合中的元素的属性名加上前缀。
+    9、typeHandler：指定一个类型处理器，用于将查询结果映射到集合中的元素。
+    10、jdbcType：指定一个JDBC类型，用于将查询结果映射到集合中的元素。
+    11、javaType：指定一个Java类型，用于将查询结果映射到集合中的元素。
+    12、column：指定一个列名，用于将查询结果映射到集合中的元素。
+    13、constructor ：指定一个构造方法，用于将查询结果映射到集合中的元素。
+    14、idArg：指定一个构造方法的参数，用于将查询结果映射到集合中的元素。
+    15、arg：指定一个构造方法的参数，用于将查询结果映射到集合中的元素。
+    16、discriminator：指定一个鉴别器，用于将查询结果映射到集合中的元素。
+    17、case：指定一个case语句，用于将查询结果映射到集合中的元素。
+    18、result：指定一个结果映射，用于将查询结果映射到集合中的元素。
+    19、association：指定一个关联关系，用于将查询结果映射到集合中的元素。
+    20、collection：指定一个集合关系，用于将查询结果映射到集合中的元素。
+
+   */
+}
---
+++ src/main/java/cn/xiaoxpai/xml/XmlSelectProperties.java
@@ -0,0 +1,42 @@
+package cn.xiaoxpai.xml;
+
+public class XmlSelectProperties {
+    /*
+      <select id="selectBlogWithPosts" resultMap="BlogResultMap">
+        select
+            b.id as blog_id, b.title as blog_title,
+            p.id as post_id, p.body as post_body
+        from blog b
+        left join post p on b.id = p.blog_id
+        where b.id = #{id}
+    </select>
+
+    select:解释：select元素用于定义一个查询语句，它有两个属性，id和resultMap，id用于指定查询语句的名字，resultMap用于指定查询结果的映射关系。
+
+    1.1 id  指定查询语句的名字，它的值是一个字符串，表示查询语句的名字。
+    1.2 parameterMap  指定查询语句的参数映射关系，它的值是一个字符串，表示参数映射的名字。
+    1.3 parameterType 指定查询语句的参数类型，它的值是一个字符串，表示参数类型的全限定名。
+    1.4 resultMap 指定查询结果的映射关系，它的值是一个字符串，表示结果映射的名字。
+    1.5 resultType 指定查询结果的类型，它的值是一个字符串，表示结果类型的全限定名。
+    1.6 resultSetType (FORWARD_ONLY | SCROLL_INSENSITIVE | SCROLL_SENSITIVE | DEFAULT)
+        指定查询的结果集类型，它的值是一个字符串，可选值有FORWARD_ONLY、SCROLL_SENSITIVE和SCROLL_INSENSITIVE，默认值是FORWARD_ONLY。
+    1.7 statementType (STATEMENT|PREPARED|CALLABLE)
+        指定查询的语句类型，它的值是一个字符串，可选值有STATEMENT、PREPARED和CALLABLE，默认值是PREPARED。
+    1.8 fetchSize 指定查询的返回行数，它的值是一个整数，表示返回行数。
+    1.9 timeout 指定查询的超时时间，它的值是一个整数，表示超时时间，单位是毫秒。
+    1.10 flushCache (true|false) 指定是否刷新缓存，它的值是一个布尔值，可选值有true和false，默认值是true。
+    1.11 useCache (true|false) 指定是否使用缓存，它的值是一个布尔值，可选值有true和false，默认值是true。
+    1.12 databaseId 指定数据库厂商的标识，它的值是一个字符串，表示数据库厂商的标识。
+    1.13 lang 指定查询语句的语言，它的值是一个字符串，表示查询语句的语言。
+    1.14 resultOrdered (true|false) 指定查询结果是否排序，它的值是一个布尔值，可选值有true和false，默认值是true。
+    1.15 resultSets 指定查询的结果集，它的值是一个字符串，表示结果集的名字。
+
+    foreach:解释 foreach元素用于定义一个foreach循环，它有两个属性，collection和item，collection用于指定集合对象，item用于指定集合元素的别名。
+    1.collection 指定集合对象，它的值是一个OGNL表达式，表示集合对象。
+    2.item 指定集合元素的别名，它的值是一个字符串，表示集合元素的别名。
+    3.index 指定集合元素的索引，它的值是一个字符串，表示集合元素的索引。
+    4.open 指定循环体的开始符号，它的值是一个字符串，表示循环体的开始符号。
+    5.close 指定循环体的结束符号，它的值是一个字符串，表示循环体的结束符号。
+    6.separator 指定循环体的分隔符，它的值是一个字符串，表示循环体的分隔符。
+   */
+}
