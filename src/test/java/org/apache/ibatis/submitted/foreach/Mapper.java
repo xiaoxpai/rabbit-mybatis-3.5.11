f3f8d4fae79fea2475cbd7cb28c2b4c7091e87f4
F1686352 <hzlh-mis-xiao.lh@foxconn.com>
2023-06-30 16:52:33 +0800

    [Mapper]  方法签名有多个list参数(#4) sql解释说明

--- src/test/java/org/apache/ibatis/submitted/foreach/Mapper.java
+++ src/test/java/org/apache/ibatis/submitted/foreach/Mapper.java
@@ -31,6 +31,17 @@ public interface Mapper {
 
   int typoInItemProperty(List<User> users);
 
+  /**
+   * 通过 select 标签定义了一条 SQL 查询语句，用于查询 users 数据表中符合特定条件的记录数。该查询语句包含一个参数：ids，它是一个列表。
+   * 在 select 标签内部，使用 foreach 标签遍历 ids 列表，每次取出一个元素，即 id。
+   * 在第一个 foreach 标签内部又嵌套了另一个 foreach 标签，用于处理第二个参数：ids2，同时生成 IN 子句所需的元素，即 #{id}。
+   * 使用 or 条件拼接第三个参数 id，如果该参数存在。
+   * 最终将查询结果存入 _int 类型的结果对象中。
+   * @param id
+   * @param ids
+   * @param ids2
+   * @return
+   */
   int itemVariableConflict(@Param("id") Integer id, @Param("ids") List<Integer> ids, @Param("ids2") List<Integer> ids2);
 
   int indexVariableConflict(@Param("idx") Integer id, @Param("idxs") List<Integer> ids, @Param("idxs2") List<Integer> ids2);
